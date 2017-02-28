9b07d2c99d8a60d167c6cfc47e42c4044456108a
kentah <kenthow@gmail.com>
2017-02-27 19:40:52 -0600

    Initial commit

---
+++ client/components/bins/BinsShare.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+
+class BinsShare extends Component {
+  onShareClick() {
+    const email = this.ref.email.value;
+
+    
+  }
+
+  render() {
+    return (
+      <footer className='bins-share'>
+        <div className='input-group'>
+          <input ref='email' className='form-control' />
+          <div className='input-group-btn'>
+            <button
+              onClick={this.onShareClick.bind(this)}
+              className='btn btn-default'
+            >
+              Share Bin
+            </button>
+          </div>
+        </div>
+      </footer>
+    )
+  }
+}
+
+export default BinsShare;
